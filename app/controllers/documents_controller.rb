2a1b1ea56a617860b15532311d320227a274c85c
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2020-12-08 21:20:59 +0800

    實作 update API

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,7 +1,34 @@
 class DocumentsController < ActionController::Base
+  $operations = []
+  $value = 'Hello, world!'
+
   def index
     respond_to do |format|
       format.html
     end
+  end
+
+  def update
+    client_revision = params[:revision]
+    client_operation = OT::TextOperation.from_a(params[:operations])
+
+    server_revision = $operations.size
+
+    if server_revision >= client_revision
+      $operations[(client_revision - 1)...server_revision].each do |other_operation|
+        client_operation = OT::TextOperation.transform(client_operation, other_operation).first
+      end
+    end
+
+    $operations << operation
+    $value = operation.apply($value)
+
+    render json: {
+      data: {
+        client_revision: client_revision,
+        server_revision: server_revision,
+        operations: operation.to_a,
+      }
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :documents, only: [:index]
+  resources :documents, only: [:index, :update]
 end
